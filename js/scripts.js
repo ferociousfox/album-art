b210ffdae31ea1e4b7194b8f9b87a1e56580a170
Ginger Lee Kretschmer and shane ryan <ginger@>
2017-12-14 09:17:28 -0800

    add form area

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+//business
+function Album(arist, album, year, image) {
+  this.artist = artistName;
+  this.album = albumTitle;
+  this.year = yearReleased;
+  this.image = coverImage;
+  this.compiler
+}
+
+//front end
+$(document).ready(function(){
+  $('#upload').click(function(event) {
+    event.preventDefault();
+    var inputName =
+    var inputArtist =
+    var inputYear =
+    var inputImg =
+    var albumItem = new Album(inputName, inputArtist, inputYear, inputImg);
+  })
+});
