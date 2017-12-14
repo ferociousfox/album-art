6377907c14b6e23639bc5151f948e02009141db3
Ginger Lee Kretschmer and shane ryan <shane@>
2017-12-14 13:07:58 -0800

    add working js and jquery logic able to add multiple image cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,46 @@
 //business
-function Album(arist, album, year, image) {
-  this.artist = artistName;
-  this.album = albumTitle;
-  this.year = yearReleased;
-  this.image = coverImage;
-  this.compiler
+
+function Album(title, artist,  year, image) {
+  this.title = title;
+  this.artist = artist;
+  this.year = year;
+  this.image = image;
+}
+
+function resetForm() {
+  $('#album-title').val("");
+  $('#artist').val("");
+  $('#year').val("");
+  $('#image').val("");
 }
 
 //front end
 $(document).ready(function(){
   $('#upload').click(function(event) {
     event.preventDefault();
-    var inputName =
-    var inputArtist =
-    var inputYear =
-    var inputImg =
-    var albumItem = new Album(inputName, inputArtist, inputYear, inputImg);
-  })
+    if (($("#album-title").val() === "") || ($("#artist").val() === "") || ($("#year").val() === "") || ($("#image").val() === "")) {
+       alert('you did not fill out one of the fields');
+       return false;
+   } else {
+      var title = $('#album-title').val();
+      var artist = $('#artist').val();
+      var year = $('#year').val();
+      var image = $('#image').val();
+      var albumItem = new Album(title, artist, year, image);
+      $('.gallery-area').append('<div class="card">' +
+                                  '<div class="card-image waves-effect waves-block waves-light">' +
+                                    '<img class="activator"' +
+                                    'src="' + albumItem.image + '">' +
+                                  '</div>' +
+                                  '<div class="card-content">' +
+                                    '<span class="card-title activator grey-text text-darken-4">' + albumItem.artist + '<i class="material-icons right">more_vert</i></span>' +
+                                  '</div>' +
+                                  '<div class="card-reveal">' +
+                                    '<span class="card-title grey-text text-darken-4">' + albumItem.title + '<i class="material-icons right">close</i></span>' +
+                                    '<p>This album was released in ' + albumItem.year + '</p>' +
+                                  '</div>'
+      );
+      resetForm();
+    };
+  });
 });
